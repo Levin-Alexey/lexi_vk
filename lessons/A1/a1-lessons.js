31de0cbb5f30f8e194b3c0ccd9125027f8df8f1a
Levin-Alexey <levin.penza@yandex.ru>
2026-06-06 09:08:22 +0300

    fix

--- lessons/A1/a1-lessons.js
+++ lessons/A1/a1-lessons.js
@@ -1,7 +1,9 @@
 // src/lessons/A1/a1-lessons.js
 
 import { A1_MODULE_1 } from "./module-1.js";
+import { A1_MODULE_2 } from "./module-2.js";
 
 export const A1_LESSONS = [
-  ...A1_MODULE_1
+  ...A1_MODULE_1,
+  ...A1_MODULE_2
 ];
